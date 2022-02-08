51dc5c4ee055affbbf3bcc2d215b3cfd4e4f98a4
Gerald H. Taranto <gh.taranto@gmail.com>
2022-02-08 14:34:48 -0100

    peak_cell

---
+++ R/peak_cell.R
@@ -0,0 +1,158 @@
+#' Identify local maxima or minima
+#'
+#' Identify local maxima or minima on a raster surface.
+#'
+#' @param attTbl data.frame, the attribute table returned by the function
+#'   \code{\link{attTbl}}.
+#' @param ngbList list, the list of neighborhoods returned by the function
+#'   \code{\link{ngbList}}.
+#' @param rNumb logic, the neighborhoods of the argument \code{ngbList} are
+#'   identified by cell numbers (\code{rNumb=FALSE}) or by row numbers
+#'   (\code{rNumb=TRUE}) (see \code{\link{ngbList}}). It is advised to use row
+#'   numbers for large rasters.
+#' @param p_col character, the column of the attribute table over which maxima
+#'   or minima are searched.
+#' @param p_fun character, if 'max' the function searches for local maxima; if
+#'   'min' the function searches for local minima.
+#' @param p_edge logic, if false local maxima or minima are not searched on edge
+#'   cells. Edge cells are considered cells on the edge of the raster and cell
+#'   neighboring NA-cells.
+#'
+#' @details A local maximum is defined as a cell on a raster surface where all
+#'   neighboring cells have smaller values.
+#'
+#'   A local minimum is defined as a cell on a raster surface where all
+#'   neighboring cells have larger or equal values.
+#'
+#' @return A \code{classVector} where peak cells are identified by the numeric
+#'   class \code{1}. See \code{\link{conditions}}) for more details about class
+#'   vectors.
+#'
+#' @export
+#'
+#' @examples
+#' # LOAD LIBRARIES
+#' library(raster)
+#' library(scapesClassification)
+#'
+#' # LOAD THE DUMMY RASTER
+#' r <- list.files(system.file("extdata", package = "scapesClassification"),
+#'                 pattern = "dummy_raster\\.tif", full.names = TRUE)
+#' r <- raster(r)
+#'
+#' # COMPUTE THE ATTRIBUTE TABLE
+#' at <- attTbl(r, "dummy_var")
+#'
+#' # COMPUTE THE LIST OF NEIGBORHOODS
+#' nbs <- ngbList(r)
+#'
+#' # PEAK.CELL
+#' ################################################################################
+#' # p_edge = FALSE
+#'
+#' pc_a <- peak.cell(attTbl = at, ngbList = nbs, rNumb = FALSE,
+#'                   p_col = "dummy_var", p_fun = "max", p_edge = FALSE)
+#'
+#' # p_edge = TRUE
+#'
+#' pc_b <- peak.cell(attTbl = at, ngbList = nbs, rNumb = FALSE,
+#'                   p_col = "dummy_var", p_fun = "max", p_edge = FALSE)
+#'
+#' # CONVERT THE CLASS VECTORS INTO RASTERS
+#' r_pca <- cv.2.rast(r, at$Cell, classVector = pc_a, plot = FALSE)
+#' r_pcb <- cv.2.rast(r, at$Cell, classVector = pc_b, plot = FALSE)
+#' ################################################################################
+#'
+#' #PLOTS
+#' ###############################################################################
+#' par(mar = c(4, 0.5, 4, 0.5), mfrow=c(1,2))
+#'
+#' # PLOT 1 - p_edge = FALSE
+#' ################################################################################
+#' plot(r_pca, axes=FALSE, box=FALSE, legend = FALSE, asp = NA,
+#'      colNA="#818792", col=c("#78b2c4", "#cfad89"))
+#' text(r)
+#' title("PEAK.CELL", adj = 0.0, line = 1,
+#'       sub = "argument: p_edge = FALSE")
+#' legend("bottomright", bg = "white",
+#'        legend = c("Peak cell", "Unclassified cells"),
+#'        fill = c("#cfad89", "#818792"))
+#' ################################################################################
+#'
+#' # PLOT 2 - p_edge = TRUE
+#' ################################################################################
+#' plot(r_pcb, axes=FALSE, box=FALSE, legend = FALSE, asp = NA,
+#'     colNA="#818792", col=c("#78b2c4", "#cfad89"))
+#' text(r)
+#' title("PEAK.CELL", adj = 0.0, line = 1,
+#'       sub = "argument: p_edge = TRUE")
+#' legend("bottomright", bg = "white",
+#'        legend = c("Peak cell", "Unclassified cells"),
+#'        fill = c("#cfad89", "#818792"))
+
+peak.cell <- function(attTbl,
+                      ngbList,
+                      rNumb = FALSE,
+                      p_col,
+                      p_fun = "max",
+                      p_edge = FALSE){
+
+
+  # TEST CELL VECTOR
+  tc <- attTbl[[p_col]]
+
+  # TEST ARGUMENT p_fun
+  if( !(p_fun %in% c("min", "max")) ){
+    stop("'p_fun' can be either 'max' or 'min'")
+  }
+
+  # TEST EDGE (CELLS WITH < 8 NEIGHBORS)
+  lnbs <- lengths(ngbList)
+
+  if(!p_edge){
+    ind_noEdge <- which(lnbs == max(lnbs))
+    ind_Edge   <- which(lnbs != max(lnbs))
+
+    tc      <- tc[ind_noEdge]
+    ngbList <- ngbList[ind_noEdge]
+
+    lnbs <- lengths(ngbList)
+  }
+
+  ## CONVERT NBS FORM CELL IDS TO CELL INDECES
+  fct  <- base::rep(seq_along(lnbs), lnbs)
+
+  if(!rNumb){
+    ngbList <- base::match(unlist(ngbList), attTbl$Cell)
+    no_nas  <- !is.na(ngbList)
+    ngbList <- ngbList[no_nas]
+    fct     <- fct[no_nas]
+
+    ngbList <- base::split(ngbList, fct)
+  }
+
+  # NEIGHBORS VECTORS
+  nb <- attTbl[[p_col]][unlist(ngbList)]
+  nb <- base::split(nb, fct)
+
+  # FIND LOCAL PEAKS
+  if(p_fun == "max"){
+    lp <- mapply(function(x,y) all(x>y), tc, nb)
+  } else {
+    lp <- mapply(function(x,y) all(x<y), tc, nb)
+  }
+
+  # TEST EDGE (CELLS WITH < 8 NEIGHBORS)
+  if(!p_edge){
+    lp0 <- logical(length = NROW(attTbl))
+    lp0[ind_noEdge] <- lp
+    lp0[!lp0] <- NA
+    lp <- as.numeric(lp0)
+
+  } else{
+    lp[!lp] <- NA
+    lp <- as.numeric(lp)
+  }
+
+  return(lp)
+}
